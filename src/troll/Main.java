9fa9e01d57b5d6f5dbe7bfd3b513c445430461b4
Martijn <martijntje7@gmail.com>
2018-04-16 13:31:28 +0200

    Add scoobeedoo

--- src/troll/Main.java
+++ src/troll/Main.java
@@ -1,5 +1,12 @@
 package troll;
 
 public class Main {
-
+	public static void main(String[] args) {
+		System.out.println("Scoo");
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Bee");
+			System.out.println("Doo");
+		}
+		System.out.println("Where are you");
+	}
 }
